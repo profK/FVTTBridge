18d041e4a83159168e10f11216aa8953d438b3d3
Jeff Kessleman <jeffpk@worldwizards.net>
2020-07-06 19:00:09 -0400

    Done through COmbatTrackerConfig.
    Moving to an automated process

---
+++ FVTTBridge/CombatEncounters.cs
@@ -0,0 +1,26 @@
+﻿using Bridge;
+using FVTTBridge.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Retyped.es2015_core;
+
+namespace FVTTBridge
+{
+    [External]
+    [Namespace(false)]
+    public class CombatEncounters
+    {
+        public static CombatEncounters Instance;
+
+        public Combat active;
+        public Array<Combat> combats;
+        [Name("object")]
+        public Type typeObj;
+        public dynamic settings;
+        public Combat viewed; //?  docs say combat encounter but there is no encounter type in API
+
+    }
+}
